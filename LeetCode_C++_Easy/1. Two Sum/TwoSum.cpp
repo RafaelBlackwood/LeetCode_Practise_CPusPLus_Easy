c858d6ef2dedce7990ff8a1265919cf77a1368f1
RafaelBlackwood <rafael.agashirinov@gmail.com>
2024-09-11 20:51:30 +0200

    finished the task fully

--- LeetCode_C++_Easy/1. Two Sum/TwoSum.cpp
+++ LeetCode_C++_Easy/1. Two Sum/TwoSum.cpp
@@ -1,12 +1,67 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm> 
 
-using namespace std;
+
+/*
+Given an array of integers nums and an integer target,
+ return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly 
+one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+
+Example 1:
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+Example 2:
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+Example 3:
+Input: nums = [3,3], target = 6
+Output: [0,1]
+*/
+class Solution {
+public:
+    std::vector<int> twoSum(std::vector<int>& nums, int target) {
+        std::vector<int> temp;
+
+        for(int i=0; i<nums.size(); i++){
+            for(int j=i+1;j<nums.size();j++){
+                if(nums[i]+nums[j]==target){
+                temp.push_back(i);
+                temp.push_back(j);
+                break;
+                }
+            }
+        }
+        
+        return temp;
+    }
+};
 
 int main()
 {
+   std::vector<int> nums = {3,2,4};
+   Solution sol;
    
+    std::vector<int> result = sol.twoSum(nums, 6); // Use the correct target
+    
+    if (!result.empty()) {
+        std::cout << "Indices: ";
+        for (int index : result) {
+            std::cout << index << " ";
+        }
+    } else {
+        std::cout << "No two sum solution found.";
+    }
+
+    std::cout << std::endl; // New line after output
+    return 0;
+
 }
-
-
